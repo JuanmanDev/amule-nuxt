61de6e92e2dffb60075bdb9efcab58c6a7e2105c
Juan Manuel Béc <Juanmabs22@hotmail.com>
2026-08-27 23:35:14 +0200

    chore: remove patch scripts

--- patch_add_link.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('test/e2e/ui.spec.ts', 'utf8');
-c = c.replace('text=Add ed2k Link', 'text=Add Link');
-fs.writeFileSync('test/e2e/ui.spec.ts', c);
-console.log('Fixed Add Link');
